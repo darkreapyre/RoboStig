25917e830a1d917c525e99dfdb34ea5f334d68c5
Trenton Potgieter <darkreapyre@gmail.com>
2018-06-09 11:05:28 -0700

    Created model template

--- modules/2_SageMakerBYOM/model.py
+++ modules/2_SageMakerBYOM/model.py
@@ -16,101 +16,29 @@ logging.getLogger("requests.packages.urllib3.connectionpool").setLevel(logging.W
 # ---------------------------------------------------------------------------- #
 #                            Training functions                                #
 # ---------------------------------------------------------------------------- #
-def train(channel_input_dirs, hyperparameters, hosts, num_gpus, output_data_dir, **kwargs):
-    # Set the Context
-    ctx = mx.gpu() if num_gpus > 0 else mx.cpu()
-    
-    # Set Local vs. Distributed training
-    if len(hosts) == 1:
-        kvstore = 'device' if num_gpus > 0 else 'local'
-    else:
-        kvstore = 'dist_device_sync' if num_gpus > 0 else 'dist_sync'
-    
-    # Load hyperparameters
-    epochs = hyperparameters.get('epochs', 11)
-    optmizer = hyperparameters.get('optmizer', 'adam')
-    lr = hyperparameters.get('learning_rate', 1.0e-4)
-    batch_size = hyperparameters.get('batch_size', 64)
-    
-    # Load Training/Testing Data
-    f_path = channel_input_dirs['training']
-    train_X, train_Y, test_X, test_Y = load_data(f_path)
-    
-    # Create Training and Test Data Iterators
-    train_data = mx.gluon.data.DataLoader(
-        mx.gluon.data.ArrayDataset(
-            train_X,
-            train_Y
-        ),
-        shuffle=True,
-        batch_size=batch_size
-    )
-    test_data = mx.gluon.data.DataLoader(
-        mx.gluon.data.ArrayDataset(
-            test_X,
-            test_Y
-        ),
-        shuffle=False,
-        batch_size=batch_size
-    )
-    
-    # Initialize the neural network structure
-    net = build_model()
-    
-    # Parameter Initialization
-    net.collect_params().initialize(mx.init.Xavier(magnitude=2.24), ctx=ctx)
-    
-    # Optimizer
-    trainer = gluon.Trainer(net.collect_params(), optmizer, {'learning_rate': lr})
-    
-    # Squared Error Loss Function
-    square_loss = gluon.loss.L2Loss()
-    
-    # Train the model
-    for epoch in range(epochs):
-        curr_loss = 0
-        for i, (data, label) in enumerate(train_data):
-            data = data.as_in_context(ctx)
-            label = label.as_in_context(ctx)
-            with autograd.record():
-                output = net(data)
-                loss = square_loss(output, label)
-            loss.backward()
-            trainer.step(data.shape[0])
-            curr_loss += nd.mean(loss).asscalar()
-        val_accuracy = accuracy(test_data, net, ctx)
-        train_accuracy = accuracy(train_data, net, ctx)
-        print("Epoch {}: Loss: {}; Training Accuracy = {}; Validation Accuracy = {}"\
-              .format(epoch, curr_loss/len(train_data), train_accuracy, val_accuracy)
-             )
 
-    # Return the model for saving
-    return net
+# BLAH BLLA BLAH
 
 def build_model():
     """
     Create the NVidia Model.
     """
-    net = gluon.nn.Sequential()
-    with net.name_scope():
-        net.add(gluon.nn.Lambda(lambda x: x/127.5-1.0)) #Normalization
-        net.add(gluon.nn.Conv2D(channels=24, kernel_size=(5, 5), strides=(2, 2), padding=1, activation='relu'))
-        net.add(gluon.nn.Conv2D(channels=36, kernel_size=(5, 5), strides=(2, 2), padding=1, activation='relu'))
-        net.add(gluon.nn.Conv2D(channels=48, kernel_size=(5, 5), strides=(2, 2), padding=1, activation='relu'))
-        net.add(gluon.nn.Conv2D(channels=64, kernel_size=3, activation='relu'))
-        net.add(gluon.nn.Conv2D(channels=64, kernel_size=3, activation='relu'))
-        net.add(gluon.nn.Flatten())
-        net.add(gluon.nn.Dense(1164))
-        net.add(gluon.nn.Activation('relu'))
-        net.add(gluon.nn.Dense(100))
-        net.add(gluon.nn.Activation('relu'))
-        net.add(gluon.nn.Dense(50))
-        net.add(gluon.nn.Activation('relu'))
-        net.add(gluon.nn.Dense(10))
-        net.add(gluon.nn.Activation('relu'))
-        net.add(gluon.nn.Dense(1))
-    net.hybridize()
+    net = None
+
     return net
+
+def load_data(f_path):
+    """
+    Retrieves and loads the training/testing data from S3.
+    
+    Arguments:
+    f_path -- Location for the training/testing input dataset.
+    
+    Returns:
+    Resized training and testing data along with training and testing labels.
+    """
+
+    return None
 
 def transform(x, y):
     """
@@ -124,41 +52,9 @@ def transform(x, y):
     x -- Numpy Array as (NCHW).
     y -- Label as Column vector.
     """
-    data  = x.reshape(-1, 3, 66, 200)
-    label = y.reshape(-1, 1)
+    data  = None
+    label = None
     return data, label
-
-def load_data(f_path):
-    """
-    Retrieves and loads the training/testing data from S3.
-    
-    Arguments:
-    f_path -- Location for the training/testing input dataset.
-    
-    Returns:
-    Pre-processed training and testing data along with training and testing labels.
-    """
-    train_x = np.load(f_path+'/train_X.npy')
-    train_y = np.load(f_path+'/train_Y.npy')
-    train_X, train_Y = transform(train_x, train_y)
-    test_x = np.load(f_path+'/valid_X.npy')
-    test_y = np.load(f_path+'/valid_Y.npy')
-    test_X, test_Y = transform(test_x, test_y)
-    return train_X, train_Y, test_X, test_Y
-
-
-def save(net, model_dir):
-    """
-    Saves the trained model to S3.
-    
-    Arguments:
-    model -- The model returned from the `train()` function.
-    model_dir -- The model directory location to save the model.
-    """
-    print("Saving the trained model ...")
-    y = net(mx.sym.var('data'))
-    y.save('%s/model.json' % model_dir)
-    net.collect_params().save('%s/model.params' % model_dir)
 
 def accuracy(data_iterator, net, ctx):
     """
@@ -179,6 +75,70 @@ def accuracy(data_iterator, net, ctx):
         predictions = nd.argmax(output, axis=1)
         acc.update(preds=predictions, labels=label)
     return acc.get()[1]
+
+def train(channel_input_dirs, hyperparameters, hosts, num_gpus, output_data_dir, **kwargs):
+    """
+    BLAH BLAH BLAH
+
+    """
+    # Set the Context
+    
+    # Set Local vs. Distributed training
+    if len(hosts) == 1:
+        kvstore = 'device' if num_gpus > 0 else 'local'
+    else:
+        kvstore = 'dist_device_sync' if num_gpus > 0 else 'dist_sync'
+    
+    # Load hyperparameters
+    
+    # Load Training/Testing Data
+    
+    # Create Training and Test Data Iterators
+    train_data = None
+    test_data = None
+    
+    # Initialize the neural network structure
+    net = build_model()
+    
+    # Parameter Initialization
+    
+    # Optimizer
+    
+    # Squared Error Loss Function
+    square_loss = None
+    
+    # Train the model
+    for epoch in range(epochs):
+        curr_loss = 0
+        for i, (data, label) in enumerate(train_data):
+            data = None
+            label = None
+            with autograd.record():
+                output = net(data)
+                loss = square_loss(output, label)
+            loss.backward()
+            trainer.step(data.shape[0])
+            curr_loss += nd.mean(loss).asscalar()
+        val_accuracy = accuracy(test_data, net, ctx)
+        train_accuracy = accuracy(train_data, net, ctx)
+        print(
+            "Epoch {}: Loss: {}; Training Accuracy = {}; Validation Accuracy = {}"\
+            .format(epoch, curr_loss/len(train_data), train_accuracy, val_accuracy)
+        )
+
+    # Return the model for saving
+    return None
+
+def save(net, model_dir):
+    """
+    Saves the trained model to S3.
+    
+    Arguments:
+    model -- The model returned from the `train()` function.
+    model_dir -- The model directory location to save the model.
+    """
+    print("Saving the trained model ...")
+    y = None
 
 # ---------------------------------------------------------------------------- #
 #                           Hosting functions                                  #
