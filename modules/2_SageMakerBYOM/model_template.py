102cd8345a355ab84fcf1adb83977ce855a203cc
Trenton Potgieter <darkreapyre@gmail.com>
2018-06-09 15:28:09 -0700

    Module 4 initial commit

--- modules/2_SageMakerBYOM/model_template.py
+++ modules/2_SageMakerBYOM/model_template.py
@@ -1,3 +1,16 @@
+# Copyright 2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
 # Import necessary libraries
 import boto3
 import os
@@ -23,7 +36,10 @@ def build_model():
     """
     Create the NVidia Model.
     """
-    net = None
+    net = gluon.nn.Sequential()
+    with net.name_scope():
+        net.add(gluon.nn.Lambda(lambda x: x/127.5-1.0)) #Normalization
+        # Complete the rest
 
     return net
 
