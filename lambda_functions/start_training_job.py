c9af0ab0b54c38695296c4b2ff19d9ecd69400ed
Trenton Potgieter <darkreapyre@gmail.com>
2018-07-02 16:44:49 -0700

    added training output

--- lambda_functions/start_training_job.py
+++ lambda_functions/start_training_job.py
@@ -91,4 +91,58 @@ def create_training_job(name, train_manifest_uri, container, s3_output_path):
         print(e)
         print('Unable to create training job.')
         raise(e)
-        
+
+"""
+{'TrainingJobName': 'sagemaker-mxnet-2018-07-02-22-03-23-633',
+ 'TrainingJobArn': 'arn:aws:sagemaker:us-west-2:500842391574:training-job/sagemaker-mxnet-2018-07-02-22-03-23-633',
+ 'ModelArtifacts': {'S3ModelArtifacts': 's3://sagemaker-us-west-2-500842391574/sagemaker-mxnet-2018-07-02-22-03-23-633/output/model.tar.gz'},
+ 'TrainingJobStatus': 'Completed',
+ 'SecondaryStatus': 'Completed',
+ 'HyperParameters': {'batch_size': '64',
+  'epochs': '12',
+  'learning_rate': '0.001',
+  'optmizer': '"adam"',
+  'sagemaker_container_log_level': '20',
+  'sagemaker_enable_cloudwatch_metrics': 'false',
+  'sagemaker_job_name': '"sagemaker-mxnet-2018-07-02-22-03-23-633"',
+  'sagemaker_program': '"model.py"',
+  'sagemaker_region': '"us-west-2"',
+  'sagemaker_submit_directory': '"s3://sagemaker-us-west-2-500842391574/sagemaker-mxnet-2018-07-02-22-03-23-633/source/sourcedir.tar.gz"'},
+ 'AlgorithmSpecification': {'TrainingImage': '520713654638.dkr.ecr.us-west-2.amazonaws.com/sagemaker-mxnet:1.1-gpu-py2',
+  'TrainingInputMode': 'File'},
+ 'RoleArn': 'arn:aws:iam::500842391574:role/SageMaker',
+ 'InputDataConfig': [{'ChannelName': 'training',
+   'DataSource': {'S3DataSource': {'S3DataType': 'S3Prefix',
+     'S3Uri': 's3://sagemaker-us-west-2-500842391574/input_data',
+     'S3DataDistributionType': 'FullyReplicated'}},
+   'CompressionType': 'None',
+   'RecordWrapperType': 'None'}],
+ 'OutputDataConfig': {'KmsKeyId': '',
+  'S3OutputPath': 's3://sagemaker-us-west-2-500842391574'},
+ 'ResourceConfig': {'InstanceType': 'ml.p3.8xlarge',
+  'InstanceCount': 1,
+  'VolumeSizeInGB': 30},
+ 'StoppingCondition': {'MaxRuntimeInSeconds': 86400},
+ 'CreationTime': datetime.datetime(2018, 7, 2, 22, 3, 25, 531000, tzinfo=tzlocal()),
+ 'TrainingStartTime': datetime.datetime(2018, 7, 2, 22, 5, 9, 50000, tzinfo=tzlocal()),
+ 'TrainingEndTime': datetime.datetime(2018, 7, 2, 22, 8, 37, 204000, tzinfo=tzlocal()),
+ 'LastModifiedTime': datetime.datetime(2018, 7, 2, 22, 8, 37, 209000, tzinfo=tzlocal()),
+ 'ResponseMetadata': {'RequestId': '32dcace2-6b1f-461c-a384-855aa06099e6',
+  'HTTPStatusCode': 200,
+  'HTTPHeaders': {'content-type': 'application/x-amz-json-1.1',
+   'date': 'Mon, 02 Jul 2018 22:53:32 GMT',
+   'x-amzn-requestid': '32dcace2-6b1f-461c-a384-855aa06099e6',
+   'content-length': '1796',
+   'connection': 'keep-alive'},
+  'RetryAttempts': 0}}
+
+
+
+from sagemaker.mxnet.model import MXNetModel
+sagemaker_model = MXNetModel(model_data = 's3://' + sagemaker_session.default_bucket() + '/model/model.tar.gz',
+                                  role = role,
+                                  entry_point = 'mnist.py')
+
+
+
+  """
