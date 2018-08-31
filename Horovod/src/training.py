13c095583b4525696b80d7719a8bd3d96c1a5d02
Trenton Potgieter <darkreapyre@gmail.com>
2018-08-31 15:45:38 -0700

    Completed main training function

--- Horovod/src/training.py
+++ Horovod/src/training.py
@@ -91,7 +91,7 @@ def train(env, hyperparameters):
         else:
             _wait_for_training_to_finish(env)
     else:
-        # for stand alon training
+        # for stand-alone training
         _run_training(env)
 
 def _change_hostname(current_host):
@@ -126,7 +126,7 @@ def _create_mpi_script(env):
     channels = framework.mapping.to_cmd_args(env.channel_input_dirs)
     #framework.modules.download_and_install(env.module_dir) # <-- check if needed
 
-    python_cmd = [sys.executable, '-m', 'mpi4py', '-m', env.module_name]
+    python_cmd = [sys.executable, 'train.py']
     python_cmd.extend(hyperparameters)
     python_cmd.extend(channels)
 
@@ -149,16 +149,6 @@ exit ${EXIT_CODE}
 def _get_master_host_name(hosts):
     return sorted(hosts)[0]
 
-def _wait_for_worker_nodes_to_start_sshd(hosts, interval=1, timeout_in_seconds=180):
-    with timeout(seconds=timeout_in_seconds):
-        while hosts:
-            logger.info("hosts that aren't SSHable yet: %s", str(hosts))
-            for host in hosts:
-                ssh_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                if _can_connect(host, 22, ssh_socket):
-                    hosts.remove(host)
-            time.sleep(interval)
-
 def _can_connect(host, port, s):
     try:
         logger.debug("testing connection to host %s", host)
@@ -170,9 +160,144 @@ def _can_connect(host, port, s):
         logger.debug("can't connect to host %s", host)
     return False
 
-#######################################################
-# TO-DO: _get_mpi_command and _run_m,pi_on_all_nodes  #
-#######################################################
+def _wait_for_worker_nodes_to_start_sshd(hosts, interval=1, timeout_in_seconds=180):
+    with timeout(seconds=timeout_in_seconds):
+        while hosts:
+            logger.info("hosts that aren't SSHable yet: %s", str(hosts))
+            for host in hosts:
+                ssh_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                if _can_connect(host, 22, ssh_socket):
+                    hosts.remove(host)
+            time.sleep(interval)
+
+def _get_mpi_command(env, hyperparameters):
+    """Constructs a command to run distributed training with MPI using mpirun.
+    Runs /mpi_script.sh on all hosts listed in the training environment. How many
+    processes in total is determined by the 'sagemaker_num_processes' hyperparameter, or one
+    per GPU, or one per CPU, as applicable. The 'sagemaker_process_slots_per_host'
+    hyperparameter can be used to override how many processes can be placed on each host.
+    Additional MPI options can be passed (and override other MPI options) using the
+    'sagemaker_additional_mpi_options' hyperparameter.
+    This command passes many options to the mpirun command:
+    * --host [host:slots]: A comma-delimited list of hosts and the number of process
+        slots on each host.
+    * -mca btl_tcp_if_include [env.network_interface_name]: Tell OpenMPI to use
+        the given network interface name for byte transfer layer communication.
+    * -mca oob_tcp_if_include [env.network_interface_name]: Tell OpenMPI to use
+        the given network interface name for out-of-band communication.
+    * -mca btl ^openib: Don't look for openib components (this just avoids a warning)
+    * -x PATH: pass $PATH from the current environment to the execution environments on remote hosts
+    * -x LD_LIBRARY_PATH: pass $LD_LIBRARY_PATH from the current environment to the execution
+        environments on remote hosts
+    * -x LD_PRELOAD=[changehostname library]: Load the changehostname library to return
+        correct values from gethostname system calls.
+    * -mca orte_abort_on_non_zero_status 1: Return a non-zero exit code if any process exits
+        with a non-zero exit code.
+    * -x NCCL_DEBUG=INFO: Enable info level logging for NCCL.
+    * -x NCCL_SOCKET_IFNAME=[env.network_interface_name]: Tell NCCL to use the given
+        network interface name for socket communication.
+    * -np [num_processes]: total number of processes to run across all nodes.
+    Args:
+        env: training environment object containing environment variables,
+                              training arguments and hyperparameters.
+    Returns:
+        str: The mpirun command to run.
+    """
+    is_gpu = env.num_gpus if env.num_gpus > 0 else 1
+
+    process_slots_per_host = int(hyperparameters.get('sagemaker_process_slots_per_host', is_gpu))
+
+    num_hosts = len(env.hosts)
+    num_processes = process_slots_per_host * num_hosts
+    num_processes = int(hyperparameters.get('sagemaker_num_processes', num_processes))
+
+    # By default, use one process per GPU, or one process per node (if training with CPU).
+    host_list = env.hosts if process_slots_per_host == 1 else \
+        [host + ':{}'.format(process_slots_per_host) for host in env.hosts]
+
+    additional_mpi_options = str(hyperparameters.get('sagemaker_additional_mpi_options', ''))
+
+    credential_vars = ['AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'AWS_SESSION_TOKEN']
+
+    logger.info('network interface name: %s', env.network_interface_name)
+
+    mpi_command = 'mpirun --allow-run-as-root --host {}'.format(",".join(host_list)) \
+                  + " -bind-to none" \
+                  + " -map-by slot" \
+                  + " -mca btl_tcp_if_include {}".format(env.network_interface_name) \
+                  + " -mca oob_tcp_if_include {}".format(env.network_interface_name) \
+                  + " -mca pml ob1" \
+                  + " -mca btl ^openib" \
+                  + " -x PATH" \
+                  + " -x LD_LIBRARY_PATH" \
+                  + " -x LD_PRELOAD={}".format(_CHANGE_HOSTNAME_LIBRARY) \
+                  + " -mca orte_abort_on_non_zero_status 1" \
+                  + " -x NCCL_DEBUG=INFO" \
+                  + " -x NCCL_SOCKET_IFNAME={}".format(env.network_interface_name) \
+                  + " -np {} ".format(num_processes)
+
+    """
+    Note: Confirm if AWS Credentials are needed
+    """
+    #for v in credential_vars:
+    #    if v in os.environ:
+    #        mpi_command += " -x {}".format(v)
+    
+    """
+    Note: It may not be nevessary to include SageMaker environment for `mpirun`,
+    since the hyperparameters are passes to the training funciton in _MPI_SCRIPT
+    and NOT using `mpi4py`
+    """
+    #for name, value in env.to_env_vars().items():
+    #    mpi_command += ' -x {}="{}"'.format(name, value)
+
+    mpi_command += " {} ".format(additional_mpi_options) + " {}".format(_MPI_SCRIPT)
+    return mpi_command
+
+
+def _run_mpi_on_all_nodes(env, hyperparameters):
+    mpi_command = _get_mpi_command(env, hyperparameters)
+    cmd = shlex.split(mpi_command)
+
+    framework.logging.log_script_invocation(cmd, env.to_env_vars(), logger)
+
+    with open(_MPI_SCRIPT) as f:
+        logger.info('Running MPI script:\n\n%s', f.read())
+    
+    subprocess.check_call(cmd)
+
+def _retry_if_false(result):
+    return result is False
+
+@retry(stop_max_delay=30 * 1000, wait_fixed=1000, retry_on_result=_retry_if_false)
+def _wait_for_mpi_to_start_running():
+    return os.path.isfile(_MPI_IS_RUNNING)
+
+@retry(wait_fixed=5000, retry_on_result=_retry_if_false)
+def _wait_until_mpi_stops_running():
+    return os.path.isfile(_MPI_IS_FINISHED)
+
+def _wait_for_training_to_finish(env):
+    current_host = env.current_host
+
+    logger.info("Worker node %s is waiting for MPI to start training process", current_host)
+    _wait_for_mpi_to_start_running()
+
+    logger.info("MPI started training process on worker node %s", current_host)
+
+    _wait_until_mpi_stops_running()
+    
+    logger.info("Training process started by MPI on worker node %s stopped", current_host)
+
+def _run_training(env):
+    logger.info('Invoking user training script.')
+
+    framework.modules.run_module(
+        env.module_dir,
+        env.to_cmd_args(),
+        env.to_env_vars(),
+        'train.py'
+    )
 
 def main():
     hyperparameters = framework.env.read_hyperparameters()
