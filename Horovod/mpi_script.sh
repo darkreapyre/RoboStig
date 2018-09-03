dc4a6ebe3c2e3e0afb378d9b89ae587ba9124abc
Trenton Potgieter <darkreapyre@gmail.com>
2018-09-03 12:59:08 -0700

    Cleanup and checkpoint

--- Horovod/mpi_script.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-touch /mpi_is_running
-/home/ec2-user/anaconda3/envs/python2/bin/python train.py --epochs 12 --learning-rate 0.0001 --training /opt/ml/input/data/training
-EXIT_CODE=$?
-touch /mpi_is_finished
-exit ${EXIT_CODE}
