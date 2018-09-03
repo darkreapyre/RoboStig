241f4e08b18b9f40be1e9d186a70364e8ad5b4ca
EC2 Default User <ec2-user@ip-172-16-48-46.us-west-2.compute.internal>
2018-09-03 18:24:23 +0000

    issues with sagemaker-container

--- Horovod/mpi_script.sh
+++ Horovod/mpi_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 touch /mpi_is_running
-/home/ec2-user/anaconda3/envs/python2/bin/python train.py --epochs 20 --learning_rate 0.01
+/home/ec2-user/anaconda3/envs/tensorflow_p27/bin/python train.py --epochs 20 --learning_rate 0.01
 EXIT_CODE=$?
 touch /mpi_is_finished
 exit ${EXIT_CODE}
